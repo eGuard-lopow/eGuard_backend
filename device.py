65f96a8b6493d861afb8767dca12ee2ef1119ece
Robin Janssens <robin@robinjanssens.net>
2018-11-27 17:00:37 +0100

    implemented knn fingerprinting localization algorithm + training code

--- device.py
+++ device.py
@@ -7,6 +7,8 @@ import base64
 import os
 import logging
 from parser import parse_alp
+import pymongo
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +19,8 @@ class Device:
     # ------------------------------
     broker_address_lora='eu.thethings.network'
     broker_address_d7='backend.idlab.uantwerpen.be'
+
+    gateway_ids=['4337313400210032', '433731340023003d', '42373436001c0037', '463230390032003e']
 
     # ------------------------------
     # Load keys
@@ -30,6 +34,15 @@ class Device:
         self.device_name = device_name
         self.device_id = device_id
         self.queue_d7 = {}                      # empty queue for dash-7 deduplication and rssi values
+        self.mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+        # db_list = self.mongo_client.list_database_names()
+        # print(db_list)
+        # if 'd7mockup' in db_list:
+        #     print('The database exists.')
+        self.db = self.mongo_client['d7mockup']
+        # print(self.db.list_collection_names())
+        self.collection = self.db['fingerprints2']
+        self.training_counter = 0
         self.processor = threading.Thread()     # empty thread object
         # ------------------------------
         # Subscribe to Dash-7
@@ -76,7 +89,12 @@ class Device:
         print('message retain flag: '+str(message.retain))
         print('raw message: '+raw)
         dict = parse_alp(raw)
-        self.queue_d7[gateway_id] = [int(dict['rx_level'])]   # save rx_level for every receiving gateway
+        print(dict)
+        self.queue_d7[gateway_id] = int(dict['rx_level'])   # save rx_level for every receiving gateway
+        # obj = {}
+        # obj['gatewayID'] = str(gateway_id)
+        # obj['rx_level'] = str(dict['rx_level'])
+        # self.queue_d7.append(obj)
         print('queue',self.queue_d7)
         print('thread',self.processor.is_alive())
         if not self.processor.is_alive():
@@ -91,7 +109,83 @@ class Device:
         time.sleep(5)
         print('-------------------- Dash-7 Process --------------------')
         print('queue',self.queue_d7)
-        # <estimate location code here>
+        if False:
+            # -------------------------
+            # Add to Training Database
+            # -------------------------
+            # x_in = input('X position >')
+            # y_in = input('Y position >')
+            x_in = 6
+            y_in = 1
+            mydict = { 'x': x_in,'y': y_in, 'gateways': self.queue_d7}
+            self.collection.insert_one(mydict)
+            print('Entry added to MongoDB')
+            self.training_counter += 1
+            print('Trainingcounter = '+str(self.training_counter))
+        if True: # else:
+            # -------------------------
+            # Localize
+            # -------------------------
+            probablistic = []
+            print('Docs:')
+            for document in self.collection.find():
+                print(document) # iterate the cursor
+                diff = []
+                for gateway_id in self.gateway_ids:
+                    if gateway_id not in self.queue_d7:
+                        self.queue_d7[gateway_id] = 200 # out of range
+                    if gateway_id not in document['gateways']:
+                        document['gateways'][gateway_id] = 200 # out of range
+                    diff.append( int(self.queue_d7[gateway_id])-int(document['gateways'][gateway_id]) )
+                print(diff)
+                rms = np.sqrt(np.mean(np.square(diff)))
+                print(rms)
+                probablistic.append({'x': document['x'],'y': document['y'],'rms':rms})
+            print(probablistic)
+            # lowest_rms = 999999  # unrealistic high
+            # x = 0
+            # y = 0
+            # for point in probablistic:
+            #     if point['rms']<lowest_rms:
+            #         lowest_rms = point['rms']
+            #         x = point['x']
+            #         y = point['y']
+
+            ordered_locations = sorted(probablistic, key = lambda i: i['rms']) # sort on RMS value
+            k = 5 # amount of neighbors
+            nearest_neighbors = ordered_locations[:k]
+            print('knn: '+str(nearest_neighbors))
+            # -------------------------
+            # Mean Value
+            # -------------------------
+
+            # for location in nearest_neighbors:
+            #     x += location['x']
+            #     y += location['y']
+            # x=x/len(x)
+            # y=x/len(x)
+
+            # -------------------------
+            # Weighted Average
+            # -------------------------
+            x = 0
+            y = 0
+            total_weight = 0
+            for location in nearest_neighbors:
+                if location['rms']==0:
+                    weight = 100
+                else:
+                    weight = 1/location['rms']
+                x += int(location['x']) * weight
+                y += int(location['y']) * weight
+                total_weight += weight
+            x=x/total_weight
+            y=y/total_weight
+            print('Location is approximately x:'+str(x)+' y:'+str(y))
+
+        # -------------------------
+        # Done
+        # -------------------------
         self.queue_d7 = {}                   # clear queue
         self.process_data(data, device_id)   # process data of first received packet
         print('--------------------------------------------------------')

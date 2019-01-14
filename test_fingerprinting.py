2393d4c23aa3cd3c03641a2cb92e5cf5fb596303
Robin Janssens <robin@robinjanssens.net>
2019-01-14 19:22:11 +0100

    added comparison of normal, exp and log

--- test_fingerprinting.py
+++ test_fingerprinting.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from localization import Localization
 
 training_db    = Localization( 'mongodb://localhost:27017/', 'd7mockup', 'training2' )
@@ -17,7 +18,38 @@ def test(k):
             # print('WRONG')
             amount_wrong += 1
     ratio = float(amount_correct)/(amount_correct+amount_wrong)
-    print('{ k: '+str(k)+' } \tCorrect answers: '+str(amount_correct)+' \tWrong answers: '+str(amount_wrong)+' \t=> '+str(round(ratio*100,2))+'%')
+    print('{ k: '+str(k)+' } \tCorrect answers: '+str(amount_correct)+' \tWrong answers: '+str(amount_wrong)+' \t=> '+str(round(ratio*100,2))+'%', end='')
+
+    amount_correct = 0
+    amount_wrong = 0
+    for document in test_db.collection.find():
+        location = training_db.localize_exp( document['gateways'], k )
+        # print('The estimated location is x:'+str(location['x'])+' y:'+str(location['y']))
+        # print('The real location is x:'+str(document['x'])+' y:'+str(document['y']))
+        if round(location['x']) == document['x'] and round(location['y']) == document['y']:
+            # print('CORRECT')
+            amount_correct += 1
+        else:
+            # print('WRONG')
+            amount_wrong += 1
+    ratio = float(amount_correct)/(amount_correct+amount_wrong)
+    print('\t{ exp } \tCorrect answers: '+str(amount_correct)+' \tWrong answers: '+str(amount_wrong)+' \t=> '+str(round(ratio*100,2))+'%', end='')
+
+    amount_correct = 0
+    amount_wrong = 0
+    for document in test_db.collection.find():
+        location = training_db.localize_log( document['gateways'], k )
+        # print('The estimated location is x:'+str(location['x'])+' y:'+str(location['y']))
+        # print('The real location is x:'+str(document['x'])+' y:'+str(document['y']))
+        if round(location['x']) == document['x'] and round(location['y']) == document['y']:
+            # print('CORRECT')
+            amount_correct += 1
+        else:
+            # print('WRONG')
+            amount_wrong += 1
+    ratio = float(amount_correct)/(amount_correct+amount_wrong)
+    print('\t{ log } \tCorrect answers: '+str(amount_correct)+' \tWrong answers: '+str(amount_wrong)+' \t=> '+str(round(ratio*100,2))+'%')
+
 
 for k in range(1,20):
     test(k)

99e8e77a666bff72cd0c76d96e0c9103aa2bd6ae
Robin Janssens <robin@robinjanssens.net>
2019-01-14 19:34:01 +0100

    commented out extra devices from fingerprinting

--- backend.py
+++ backend.py
@@ -6,10 +6,10 @@ import threading
 # Create devices
 # ------------------------------
 device_list = []
-device_list.append(threading.Thread(target=Device, args=[ 'octa-robin', '493332340046001e', 0, [4,0] ]).start())
+device_list.append(threading.Thread(target=Device, args=[ 'octa-robin',  '493332340046001e', 0, [2,2] ]).start())
 device_list.append(threading.Thread(target=Device, args=[ 'octa-thomas', '4933323400280025', 0, [999,999] ]).start())
-device_list.append(threading.Thread(target=Device, args=[ 'foo-joris',  '4933323400370020', 0, [60,1] ]).start())
-device_list.append(threading.Thread(target=Device, args=[ 'foo-wesley', '49333234002e0020', 1, [4,0] ]).start())
+# device_list.append(threading.Thread(target=Device, args=[ 'foo-joris',   '4933323400370020', 0, [60,1] ]).start())
+# device_list.append(threading.Thread(target=Device, args=[ 'foo-wesley',  '49333234002e0020', 0, [4,0] ]).start())
 
 # ------------------------------
 # Loop

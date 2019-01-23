89c459ab455efdabc5afc4fec8a39b3fbd7b8ea4
Robin Janssens <robin@robinjanssens.net>
2019-01-23 16:12:52 +0100

    added octa-toon

--- backend.py
+++ backend.py
@@ -6,10 +6,9 @@ import threading
 # Create devices
 # ------------------------------
 device_list = []
-device_list.append(threading.Thread(target=Device, args=[ 'octa-robin',  '493332340046001e', 0, [2,2] ]).start())
+device_list.append(threading.Thread(target=Device, args=[ 'octa-robin',  '493332340046001e', 0, [2,2] ]).start()) # args=[ device_name, device_id, training_mode, training_location[x,y] ]
 device_list.append(threading.Thread(target=Device, args=[ 'octa-thomas', '4933323400280025', 0, [999,999] ]).start())
-# device_list.append(threading.Thread(target=Device, args=[ 'foo-joris',   '4933323400370020', 0, [60,1] ]).start())
-# device_list.append(threading.Thread(target=Device, args=[ 'foo-wesley',  '49333234002e0020', 0, [4,0] ]).start())
+device_list.append(threading.Thread(target=Device, args=[ 'octa-toon', '0010962A80AEB62D', 0, [999,999] ]).start())
 
 # ------------------------------
 # Loop

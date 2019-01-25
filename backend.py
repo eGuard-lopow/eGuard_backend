fa964139011ce4133866e1a71e2708090af2a229
Robin Janssens <robin@robinjanssens.net>
2019-01-25 14:01:40 +0100

    fixed wrong hardware id

--- backend.py
+++ backend.py
@@ -8,7 +8,7 @@ import threading
 device_list = []
 device_list.append(threading.Thread(target=Device, args=[ 'octa-robin',  '493332340046001e', 0, [2,2] ]).start()) # args=[ device_name, device_id, training_mode, training_location[x,y] ]
 device_list.append(threading.Thread(target=Device, args=[ 'octa-thomas', '4933323400280025', 0, [999,999] ]).start())
-device_list.append(threading.Thread(target=Device, args=[ 'octa-toon', '0010962A80AEB62D', 0, [999,999] ]).start())
+device_list.append(threading.Thread(target=Device, args=[ 'octa-toon', '49333234002a001f', 0, [999,999] ]).start())
 
 # ------------------------------
 # Loop
